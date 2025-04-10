1f33ca702d4333a8955741cd90c2151092123848
Sonuo7 <rockysingh12ops@gmail.com>
2025-04-10 14:47:09 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,13 +1,13 @@
 from os import environ
 
-API_HASH = environ.get("API_HASH", "")
-API_ID = int(environ.get("API_ID", ""))
+API_HASH = environ.get("API_HASH", ""3b33568aa7119719d1f37cb15b3ae587)
+API_ID = int(environ.get("API_ID", "28137469"))
 BOT_TOKEN = environ.get("BOT_TOKEN", "")
-BOT_OWNER = int(environ.get("BOT_OWNER", ""))
-BOT_USERNAME = environ.get("BOT_USERNAME", "QuickReactRobot")
-LOG_CHANNEL = int(environ.get("LOG_CHANNEL", ""))
-AUTH_CHANNEL = int(environ.get("AUTH_CHANNEL", ""))
-DATABASE_URL = environ.get("DATABASE_URL", "")
+BOT_OWNER = int(environ.get("BOT_OWNER", "897584437"))
+BOT_USERNAME = environ.get("BOT_USERNAME", "Nazriyarection_bot")
+LOG_CHANNEL = int(environ.get("LOG_CHANNEL", "-1002169575469"))
+AUTH_CHANNEL = int(environ.get("AUTH_CHANNEL", "-1001882358473"))
+DATABASE_URL = environ.get("DATABASE_URL", "mongodb+srv://sonuoj:sonuhij@cluster0.a9dkw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 
 # Define default emojis list
 EMOJIS = [
