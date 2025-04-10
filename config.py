2f4c9cb80101eb58859bbdf380df85b24dbc5a9e
Sonuo7 <rockysingh12ops@gmail.com>
2025-04-10 15:10:32 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 from os import environ
 
-API_HASH = environ.get("API_HASH", ""3b33568aa7119719d1f37cb15b3ae587)
+API_HASH = environ.get("API_HASH", "3b33568aa7119719d1f37cb15b3ae587")
 API_ID = int(environ.get("API_ID", "28137469"))
 BOT_TOKEN = environ.get("BOT_TOKEN", "")
 BOT_OWNER = int(environ.get("BOT_OWNER", "897584437"))
