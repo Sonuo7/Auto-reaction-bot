cff8e94aae087da1a5fcdb77aa38fe1159280e27
Sonuo7 <rockysingh12ops@gmail.com>
2025-04-10 14:53:14 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ BOT_TOKEN = environ.get("BOT_TOKEN", "")
 BOT_OWNER = int(environ.get("BOT_OWNER", "897584437"))
 BOT_USERNAME = environ.get("BOT_USERNAME", "Nazriyarection_bot")
 LOG_CHANNEL = int(environ.get("LOG_CHANNEL", "-1002169575469"))
-AUTH_CHANNEL = int(environ.get("AUTH_CHANNEL", "-1001882358473"))
+AUTH_CHANNEL = int(environ.get("AUTH_CHANNEL", "-1002189546391"))
 DATABASE_URL = environ.get("DATABASE_URL", "mongodb+srv://sonuoj:sonuhij@cluster0.a9dkw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 
 # Define default emojis list
